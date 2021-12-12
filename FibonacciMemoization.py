1e6bc9f0b7ec20ad31af5ad9d58ccb55e91011da
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-12-12 21:28:53 +0530

    fib with memoization

---
+++ FibonacciMemoization.py
@@ -0,0 +1,11 @@
+def fibMemo(n,memo):
+    if n==1:
+        return 0
+    if n==2:
+        return 1
+    if n not in memo:
+        memo[n]=fibMemo(n-1,memo)+fibMemo(n-2,memo)
+    return memo[n]
+
+myDict={}
+print(fibMemo(6,myDict))
